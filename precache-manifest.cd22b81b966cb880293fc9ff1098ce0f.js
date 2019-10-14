39aac0f3ad3d3b0ef9215604b9fc2c597fc910dc
carolgmonteiro <carolgmonteiro@live.com>
2019-10-14 16:59:40 -0300

    Updates

---
+++ precache-manifest.cd22b81b966cb880293fc9ff1098ce0f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b2100de8ff0c80f635476dae6c9c00",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "658d34e1c3f970997d37",
+    "url": "/portfolio/static/css/2.9a008ecd.chunk.css"
+  },
+  {
+    "revision": "92e81516841c60ccdb33",
+    "url": "/portfolio/static/css/main.6865a005.chunk.css"
+  },
+  {
+    "revision": "658d34e1c3f970997d37",
+    "url": "/portfolio/static/js/2.144c061a.chunk.js"
+  },
+  {
+    "revision": "92e81516841c60ccdb33",
+    "url": "/portfolio/static/js/main.d9e7af22.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  },
+  {
+    "revision": "5337fdd42a94eb7df557d19c30c69c71",
+    "url": "/portfolio/static/media/background-home-03.5337fdd4.svg"
+  },
+  {
+    "revision": "5453c2aad7aed87a60fccb3ea7897d7c",
+    "url": "/portfolio/static/media/button-home-04.5453c2aa.svg"
+  },
+  {
+    "revision": "7ca664e1ebd8ec557c19ed90bf124fcb",
+    "url": "/portfolio/static/media/css3.7ca664e1.svg"
+  },
+  {
+    "revision": "f73adebd33a733fe8c2f0d7d372f75dc",
+    "url": "/portfolio/static/media/design-icon.f73adebd.svg"
+  },
+  {
+    "revision": "bef9ec59616272e78b6a697749ee3b65",
+    "url": "/portfolio/static/media/fire.bef9ec59.svg"
+  },
+  {
+    "revision": "1254d5817c6c33b83bc285b57f78d56d",
+    "url": "/portfolio/static/media/github-icon.1254d581.svg"
+  },
+  {
+    "revision": "bfdc1d1405bde89e4a2bfba805c7bc7f",
+    "url": "/portfolio/static/media/github.bfdc1d14.svg"
+  },
+  {
+    "revision": "0d4d36589622bf38fd61c62dd42134c9",
+    "url": "/portfolio/static/media/html5.0d4d3658.svg"
+  },
+  {
+    "revision": "d337ac987662f86e59e1f4ed9e6baafa",
+    "url": "/portfolio/static/media/icon-carol-02.d337ac98.svg"
+  },
+  {
+    "revision": "7adacde49cbe3f051326d5d82a3af2c6",
+    "url": "/portfolio/static/media/icon-carol_01.7adacde4.svg"
+  },
+  {
+    "revision": "e335794c806d6f77f8ce0d6dae289ae4",
+    "url": "/portfolio/static/media/js.e335794c.svg"
+  },
+  {
+    "revision": "3721dc85d39e5a7881255d0648feb60a",
+    "url": "/portfolio/static/media/linkedin-icon.3721dc85.svg"
+  },
+  {
+    "revision": "e145e1a438f066328681a95a6f0407e3",
+    "url": "/portfolio/static/media/proj-burgerqueen.e145e1a4.PNG"
+  },
+  {
+    "revision": "7687aa4a8c15883665f4408b934b80ba",
+    "url": "/portfolio/static/media/proj-pokemon.7687aa4a.PNG"
+  },
+  {
+    "revision": "e1d40e540d5b8ed52075aabea5286bbe",
+    "url": "/portfolio/static/media/proj-social network.e1d40e54.PNG"
+  },
+  {
+    "revision": "0d8acb1bc7ca76376b24877e31a5ccc1",
+    "url": "/portfolio/static/media/proj-viewnow.0d8acb1b.PNG"
+  },
+  {
+    "revision": "d0391bda54a0bab321af7f1e3f82a0c5",
+    "url": "/portfolio/static/media/react.d0391bda.svg"
+  }
+]);
